99bffccef4c401f71b5fb40f8683d6a1944dca3d
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-12-25 17:38:45 -0500

    Se separa la logica de creación de contenido en contentManagement

---
+++ services/contentManagement.service.js
@@ -0,0 +1,47 @@
+const { sequelize } = require('./../libs/sequelize');
+const Transactional = require('./Transactional.service');
+const boom = require('@hapi/boom');
+
+const generateLink = require('./../utils/manipulation/link')
+const readingTime = require('./../utils/manipulation/readingTime')
+const ClassificationAssociation = require('./classificationAssociation.service');
+
+const serviceClassificationAssociation = new ClassificationAssociation();
+
+class ContentManagement extends Transactional {
+    async create(body, model, nameModel, transaction){
+        try {
+            const link = await generateLink(body.title, model, sequelize);
+            const createContentManagement = await sequelize.models[model].create({...body, link: link, reading_time:readingTime(body.content)}, {transaction});
+            const createClassification = await serviceClassificationAssociation.createClassificationOfModel(body.categories, body.subcategories, body.tags, model, {[nameModel]: createContentManagement.id}, transaction);
+            return createContentManagement
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async upate(body, id, model, nameModel, transaction){
+        try{
+            const contentManagement = await this.getElementById(id, model);
+            if(body.title){
+                body.link = await generateLink(body.title, model, sequelize);
+            }
+            const updateContentManagement = await contentManagement.update(body, {transaction});
+            const updateContentManagementClassification = await serviceClassificationAssociation.updateClassificationModelType(body.categories, body.subcategories, body.tags, body.idsEliminateCategories, body.idsEliminateSubcategories, body.idsEliminateTags, model, {[nameModel]: contentManagement.id}, id, transaction);
+        } catch (error){
+            throw error
+        }
+    }
+
+    async delete(id, model, fieldNameElement, transaction){
+        try {
+            const contentManagement = await this.getElementById(id, model);
+            const deleteContentManagementClassification = await serviceClassificationAssociation.deleteClassificationOfModel(null, null, null, model, transaction, contentManagement.id, fieldNameElement, true);
+            const deleteContentManagement = await contentManagement.destroy({transaction})
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+module.exports = ContentManagement;
--- services/publications.service.js
+++ services/publications.service.js
@@ -2,30 +2,15 @@ const { sequelize } = require('./../libs/sequelize');
 const Transactional = require('./Transactional.service');
 const boom = require('@hapi/boom');
 
-const generateLink = require('./../utils/manipulation/link')
-const readingTime = require('./../utils/manipulation/readingTime')
-const ClassificationAssociation = require('./classificationAssociation.service');
+const ContentManagement = require('./contentManagement.service')
 
-const serviceClassificationAssociation = new ClassificationAssociation();
+const serviceContentManagement = new ContentManagement();
 
 class Publications extends Transactional {
-    async getPublication(id, include = null){
-        return this.withTransaction(async (transaction) => {
-            const publication = await sequelize.models.Publications.findByPk(id, {
-                include: include
-            });
-                if(!publication){
-                    throw boom.notFound('Publicación no encontrada');
-                }
-                return publication;
-        })
-    }
 
     async create(body, transaction){
         try {
-            const link = await generateLink(body.title, 'Publications', sequelize);
-            const createPublication = await sequelize.models.Publications.create({...body, link: link, reading_time:readingTime(body.content)}, {transaction});
-            const createClassification = await serviceClassificationAssociation.createClassificationOfModel(body.categories, body.subcategories, body.tags, 'Publications', {publicationId: createPublication.id}, transaction);
+            const createPublication = await serviceContentManagement.create(body, 'Publications', 'publicationId', transaction)
             return createPublication
         } catch (error) {
             throw error
@@ -34,12 +19,7 @@ class Publications extends Transactional {
 
     async upate(body, id, transaction){
         try{
-            const publication = await this.getPublication(id);
-            if(body.title){
-                body.link = await generateLink(body.title, 'Publications', sequelize);
-            }
-            const updatePublication = await publication.update(body, {transaction});
-            const updatePublicationClassification = await serviceClassificationAssociation.updateClassificationModelType(body.categories, body.subcategories, body.tags, body.idsEliminateCategories, body.idsEliminateSubcategories, body.idsEliminateTags, 'Publications', {publicationId: publication.id}, id, transaction);
+            const updatePublication = await serviceContentManagement.upate(body, id, 'Publications', 'publicationId', transaction);
         } catch (error){
             throw error
         }
@@ -47,9 +27,7 @@ class Publications extends Transactional {
 
     async delete(id, transaction){
         try {
-            const publication = await this.getPublication(id);
-            const deletePublicationClassification = await serviceClassificationAssociation.deleteClassificationOfModel(null, null, null, 'Publications', transaction, publication.id, true);
-            const deletePublication = await publication.destroy({transaction})
+            const deletePublicationClassification = await serviceContentManagement.delete(id, 'Publications', 'publicationId', transaction);
         } catch (error) {
             throw error
         }
