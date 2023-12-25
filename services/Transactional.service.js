8e5716c326e2c49290b227fbf149e6909be681f6
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-12-25 17:39:41 -0500

    Se crean metodos para traer información de la base de datos

--- services/Transactional.service.js
+++ services/Transactional.service.js
@@ -1,4 +1,5 @@
 const { sequelize } = require('./../libs/sequelize');
+const boom = require('@hapi/boom');
 
 class Transactional {
     async withTransaction(callback) {
@@ -12,6 +13,38 @@ class Transactional {
             if (transaction) await transaction.rollback();
             throw error;
         }
+    }
+    async getElementById(id, model, include = null){
+        return this.withTransaction(async (transaction) => {
+            const element = await sequelize.models[model].findByPk(id, {
+                include: include
+            });
+            if(!element){
+                throw boom.notFound(`${model} no encontrado`);
+            }
+            return element;
+        })
+    }
+
+    async getElementWithCondicional(model, where, message, include=null){
+        const element = await sequelize.models[model].findOne({
+            where: { ...where },
+            include: include
+        })
+        if(!element){
+            throw boom.unauthorized(message);
+        }
+        return element;
+    }
+
+    async getAllElements(model, where = null, include = null){
+        return this.withTransaction(async (transaction) => {
+            const elements = await sequelize.models[model].findAll({
+                where: { ...where },
+                include: include
+            })
+            return elements
+        });
     }
     async checkModel(model, nameModel){
         try {
