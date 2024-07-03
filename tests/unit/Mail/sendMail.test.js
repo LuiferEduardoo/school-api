e7e1324629d89f210068ebf8b47f17f50a05b8a5
Luifer <luifer01ortegaperez@gmail.com>
2024-07-03 13:58:52 -0500

    test: unit test emails.service

---
+++ tests/unit/Mail/sendMail.test.js
@@ -0,0 +1,68 @@
+// __tests__/mailer.test.js
+
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path');
+const { SendMain } = require('../../../services/emails.service');  // Ajusta la ruta según tu estructura de archivos
+const { config } = require('../../../config/config');
+
+jest.mock('nodemailer', () => ({
+    createTransport: jest.fn().mockReturnValue({
+      sendMail: jest.fn().mockReturnValue((mailoptions, callback) => {})
+    })
+  }));
+jest.mock('ejs');
+
+
+describe('SendMail', () => {
+    const email = 'test@example.com';
+    const subject = 'Test Subject';
+    const date = { key: 'value' };
+    
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should render the correct template and send email for admissionRequest', async () => {
+        const templatePath = path.join(__dirname, '../../../views/admissionRequest.ejs');
+        ejs.renderFile.mockResolvedValue('<html>Admission Request</html>');
+
+        await SendMain(email, subject, 'admissionRequest', date);
+
+        expect(ejs.renderFile).toHaveBeenCalledWith(templatePath, date);
+    });
+
+    test('should render the correct template and send email for admissionDecision', async () => {
+        const templatePath = path.join(__dirname, '../../../views/admissionDecision.ejs');
+        ejs.renderFile.mockResolvedValue('<html>Admission Decision</html>');
+
+        await SendMain(email, subject, 'admissionDecision', date);
+
+        expect(ejs.renderFile).toHaveBeenCalledWith(templatePath, date);
+    });
+
+    test('should render the correct template and send email for recoveryPassword', async () => {
+        const templatePath = path.join(__dirname, '../../../views/recoveryPassword.ejs');
+        ejs.renderFile.mockResolvedValue('<html>Recovery Password</html>');
+
+        await SendMain(email, subject, 'recoveryPassword', date);
+
+        expect(ejs.renderFile).toHaveBeenCalledWith(templatePath, date);
+    });
+
+    test('should render the correct template and send email for changePassword', async () => {
+        const templatePath = path.join(__dirname, '../../../views/changePassword.ejs');
+        ejs.renderFile.mockResolvedValue('<html>Change Password</html>');
+
+        await SendMain(email, subject, 'changePassword', date);
+
+        expect(ejs.renderFile).toHaveBeenCalledWith(templatePath, date);
+    });
+
+    test('should not send email for an unknown type', async () => {
+        await expect(
+            SendMain(email, subject, 'unknownType', date)
+        ).rejects.toThrow('Type does not have a template');
+    });
+});
