e7ea7500ba0d8692219428c0469ed53e5a17479c
Luifer <luifer01ortegaperez@gmail.com>
2024-07-03 17:53:51 -0500

    fix: corrección en el cálculo del tiempo de lectura

--- utils/manipulation/readingTime.js
+++ utils/manipulation/readingTime.js
@@ -1,7 +1,7 @@
 const convertMinutesToTime = require('./convertMinutesToTime');
 
 const readingTime = (text) => {
-    const wordCount = text.split(/\s+/).length;
+    const wordCount = text.length > 0 ? text.split(/\s+/).length : 0;
     const wordsPerMinute = 225; // An average words read per minute
     const readingTimeMinutes = Math.ceil(wordCount / wordsPerMinute);
     return convertMinutesToTime(readingTimeMinutes);
