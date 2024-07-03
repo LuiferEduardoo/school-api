835ebe902010358b3b7edcb2432d6e669f9759ac
Luifer <luifer01ortegaperez@gmail.com>
2024-07-03 17:50:09 -0500

    fix: corrección en la conversión de minutos con segundos

--- utils/manipulation/convertMinutesToTime.js
+++ utils/manipulation/convertMinutesToTime.js
@@ -1,6 +1,6 @@
 const convertMinutesToTime = (minutes) => {
     const hours = Math.floor(minutes / 60);
-    const remainingMinutes = Math(minutes % 60);
+    const remainingMinutes = Math.floor(minutes % 60);
     const seconds = Math.round((minutes % 1) * 60);
 
     return `${hours.toString().padStart(2, '0')}:${remainingMinutes
