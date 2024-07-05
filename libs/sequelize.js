422e0fe747ce900d00a84d94c9ae87eda9c79fcc
Luifer <luifer01ortegaperez@gmail.com>
2024-07-04 23:13:43 -0500

    perf: configura Sequelize para utilizar SQLite en pruebas

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,14 +3,24 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const setupModels = require('./../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+let sequelize;
 
-const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
-    logging: false,
-});
+if (process.env.NODE_ENV === 'test') {
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: ':memory:', // Utiliza SQLite en memoria para pruebas
+        logging: false,
+    });
+} else {
+    const USER = encodeURIComponent(config.dbUser);
+    const PASSWORD = encodeURIComponent(config.dbPassword);
+    const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+    sequelize = new Sequelize(URI, {
+        dialect: 'postgres',
+        logging: false,
+    });
+}
 
 setupModels(sequelize);
 
