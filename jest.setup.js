7ecb948fd494fcf3896bf0e1927a14da2a36f0b3
Luifer <luifer01ortegaperez@gmail.com>
2024-07-10 23:33:12 -0500

    perf: configuración del tiempo de espera de Jest a 10 segundos

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,8 @@
 process.env.NODE_ENV = 'test';
 process.env.JWT_SECRET_RECOVERY_PASSWORD = '37`W?;N:m"u"N2RGSZ5o3.i^n0X';
-process.env.JWT_SECRET_REFRESH_TOKEN = 'W3_wipto>O}4.7h(br^S9hmCUCY'
+process.env.JWT_SECRET_REFRESH_TOKEN = 'W3_wipto>O}4.7h(br^S9hmCUCY';
 process.env.JWT_SECRET_ACCESS_TOKEN = '(g:?M^ySs2|Wz&3y-F1K7l`3H-}';
+
+module.exports = {
+    testTimeout: 10000, // 10 segundos para todos los tests
+};
