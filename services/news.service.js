00e8cf6b1bb02c80fdbddf076892a17f0e2a3eac
Luifer <luifer01ortegaperez@gmail.com>
2024-07-23 22:07:30 -0500

    fix(news): corregir error que limitaba la visualización de elementos a dos

--- services/news.service.js
+++ services/news.service.js
@@ -10,40 +10,49 @@ const serviceImageAssociation = new ImageAssociation();
 
 class News extends Transactional {
     async get(id, link, req){
-        const where = this.checkPermissionToGet(req)
-        const query = this.queryParameterPagination(req.query);
-        const { search, important, visible } = req.query;
-        const whereClause = {};
-        const dataFilter = ['$publication.title$', '$publication.categories.categories.clasification.name$', '$publication.subcategories.subcategories.clasification.name$', '$publication.tags.tags.clasification.name$']
-        const include = [
-            { association: 'publication', where: where, order: this.order, include: [
-                {association: 'categories', include:[{association: 'categories', include: 'clasification'}]},
-                {association:'subcategories', include:[{association: 'subcategories', include: 'clasification'}]},
-                {association:'tags', include:[{association: 'tags', include: 'clasification'}]}
-            ]},
-            { association: 'imageNews', include: [{association: 'image', include: 'file'}] }
-        ]
-        this.querySearch(dataFilter, search, whereClause);
-
-        this.handleElementPrivacy(req, where, '$publication.visible$', visible);
-        
-        if (important) {
-            whereClause['$publication.important$'] = important;
-        }
-
-        return this.withTransaction(async (transaction) => {
-            if(id || link){
-                const where = id ? {id: id} : {'$publication.link$': link}
-                return await this.getElementWithCondicional('NewsPublications', include, where);
+        try {
+            const where = this.checkPermissionToGet(req)
+            const query = this.queryParameterPagination(req.query);
+            const { search, important, visible } = req.query;
+            const whereClause = {};
+            const dataFilter = ['$publication.title$']
+            const include = [
+                { association: 'publication', required: true, where: where, order: this.order},
+                { association: 'imageNews', include: [{association: 'image', include: 'file'}] }
+            ];
+            
+            this.querySearch(dataFilter, search, whereClause);
+    
+            this.handleElementPrivacy(req, where, '$publication.visible$', visible);
+            
+            if (important) {
+                whereClause['$publication.important$'] = important;
             }
-            return await this.getAllElements('NewsPublications', whereClause, include, null, query )
-        })
+
+            if(id || link){
+                const whereOne = id ? {id: id} : {'$publication.link$': link}
+                const includeOneElement = [
+                    { association: 'publication', where: where, order: this.order, include: [
+                        {association: 'categories', include:[{association: 'categories', include: 'clasification'}]},
+                        {association:'subcategories', include:[{association: 'subcategories', include: 'clasification'}]},
+                        {association:'tags', include:[{association: 'tags', include: 'clasification'}]}
+                    ]},
+                    { association: 'imageNews', include: [{association: 'image', include: 'file'}] }
+                ];
+
+                return await this.getElementWithCondicional('NewsPublications', includeOneElement, whereOne);
+            }
+            return await this.getAllElements('NewsPublications', whereClause, include, null, query );
+            
+        } catch(error){
+            throw error
+        }
     }
     async create(req, body){
         return this.withTransaction(async (transaction) => {
             const createPublication = await servicePublications.create(body,transaction);
             const newsPublications = await sequelize.models.NewsPublications.create({publicationId: createPublication.id, userId: req.user.sub}, {transaction});
-            const imagesNewsPublications = await serviceImageAssociation.createOrAdd(req, 'ImageNews', {newsPublicationsId: newsPublications.id}, `news/${newsPublications.id}`, body.idImage, transaction)
+            const imagesNewsPublications = await serviceImageAssociation.createOrAdd(req, 'ImageNews', {newsPublicationsId: newsPublications.id}, `news/${newsPublications.id}`, body.idImage, transaction, true);
             return {
                 message: 'Noticia creada con exito'
             }
@@ -54,7 +63,7 @@ class News extends Transactional {
         return this.withTransaction(async (transaction) => {
             const getNewsPublications = await this.getElementById(id, 'NewsPublications');
             const updateNewsPublications = await servicePublications.upate(body, getNewsPublications.publicationId, transaction);
-            const updateimagesNewsPublications = await serviceImageAssociation.update(req, 'ImageNews', {newsPublicationsId: id}, body.idNewImage, `news/${id}`, body.idImageEliminate, body.eliminateImage, transaction);
+            const updateimagesNewsPublications = await serviceImageAssociation.update(req, 'ImageNews', {newsPublicationsId: id}, body.idNewImage, `news/${id}`, body.idImageEliminate, body.eliminateImage, transaction, true);
             return {
                 message: 'Noticia actualizada con exito'
             }
